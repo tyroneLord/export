398c223fa46e5d66ae05844ee94d6d7e677545dc
Tyrone Gaylord <tyronegaylord@Digitals-MacBook-Pro.local>
2013-07-23 11:05:38 -0500

    set up file to export csv and xls format

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.order(:name)
+    respond_to do |format|
+    	format.html
+    	format.csv {send_data @products.to_csv} #sned_data will make the browser downlad the file
+    	# format.csv {render text: @products.to_csv} #render text: makes the page so data #the @products.to_csv is done in the model
+    	format.xls #{ send_data @products.to_csv(col_sep: "\t") } #becuase we set up new type in initializers/mime_types.rb i can now format.xls the col_sep make the file tab delimited
+    end
+  end
+
+end
